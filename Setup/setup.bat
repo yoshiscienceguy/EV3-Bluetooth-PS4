33a82ca766a6cc43e341093d2bb7076a9eaafafc
Fernando de Paz <f.depaz@hotmail.com>
2024-12-26 12:23:12 -0800

    Update setup.bat

--- Setup/setup.bat
+++ Setup/setup.bat
@@ -1,3 +1,5 @@
+cd /d %~dp0
+
 vs_buildtools.exe --norestart --passive --wait --downloadThenInstall --includeRecommended --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools
 
 mklink C:\Program Files (x86)\Windows Kits\10\Lib\10*\um\x64\irprops.lib C:\Program Files (x86)\Windows Kits\10\Lib\10*\um\x64\bthprops.lib
