9dbbb66777969e37b15e213721a466034bb25ac3
Fernando de Paz <f.depaz@hotmail.com>
2024-12-26 12:55:22 -0800

    Update setup.bat

--- Setup/setup.bat
+++ Setup/setup.bat
@@ -1,8 +1,10 @@
 cd /d %~dp0
 
 vs_buildtools.exe --norestart --passive --wait --downloadThenInstall --includeRecommended --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools
+cd C:\Program Files (x86)\Windows Kits\10\Lib\10*\um\x64\
+mklink irprops.lib bthprops.lib
 
-mklink C:\Program Files (x86)\Windows Kits\10\Lib\10*\um\x64\irprops.lib C:\Program Files (x86)\Windows Kits\10\Lib\10*\um\x64\bthprops.lib
+cd /d %~dp0
 
 pip install -r requirements.txt
 pip install setuptools==57.0.0
